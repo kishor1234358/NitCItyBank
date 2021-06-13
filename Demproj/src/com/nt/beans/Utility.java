835b3282590650a4197d9cd0d51123a42658f705
dev <dev1@gmail.com>
2021-06-13 10:22:12 +0530

    dev modifid m1

--- Demproj/src/com/nt/beans/Utility.java
+++ Demproj/src/com/nt/beans/Utility.java
@@ -3,7 +3,7 @@ package com.nt.beans;
 public class Utility {
 
 	public void m1() {
-		System.out.println("utility");
+		System.out.println("utility abcd" );
 	}
 	
 	public static void main(String[] args) {
