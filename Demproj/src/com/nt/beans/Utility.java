0161e79f76923a00a5e962a7359beaa9c6eb1f3b
dev <dev1@gmail.com>
2021-06-13 10:18:14 +0530

    dev added m1 method

--- Demproj/src/com/nt/beans/Utility.java
+++ Demproj/src/com/nt/beans/Utility.java
@@ -2,6 +2,10 @@ package com.nt.beans;
 
 public class Utility {
 
+	public void m1() {
+		System.out.println("utility");
+	}
+	
 	public static void main(String[] args) {
 	System.out.println("kishore");
 	
